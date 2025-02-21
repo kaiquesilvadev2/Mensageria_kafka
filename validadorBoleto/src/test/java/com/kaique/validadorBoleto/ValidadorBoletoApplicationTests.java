7bb0cc350f01f75992857c33a73dc22869f1c96c
kaiquesilvadev2 <j.kaiquesilva1570@gmail.com>
2025-02-21 20:14:59 -0300

    criação projeto validadorBoleto

---
+++ validadorBoleto/src/test/java/com/kaique/validadorBoleto/ValidadorBoletoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaique.validadorBoleto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidadorBoletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
